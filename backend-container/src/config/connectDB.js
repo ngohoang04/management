e0242451d310653191fd001390908a3003468c38
Trong Hoang <hoangngotrong2004@gmail.com>
2025-08-28 13:59:06 +0700

    test connect

---
+++ backend-container/src/config/connectDB.js
@@ -0,0 +1,24 @@
+import { Sequelize } from 'sequelize';
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME || 'hoidanit',
+    process.env.DB_USER || 'root',
+    process.env.DB_PASS || '',
+    {
+        host: process.env.DB_HOST || 'localhost',
+        dialect: 'mysql',
+        logging: false
+    }
+);
+
+const connectDB = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+};
+
+
+export default connectDB;
